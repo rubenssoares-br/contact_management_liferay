782221d76cc75cefc639624ad74fe0645888dd44
Rubens Soares <rubens.soares@liferay.com>
2024-11-26 17:11:32 -0300

    Implementing an Add Method for the Contact entity

--- modules/contact/contact-service/src/main/java/com/liferay/contact/management/service/impl/ContactLocalServiceImpl.java
+++ modules/contact/contact-service/src/main/java/com/liferay/contact/management/service/impl/ContactLocalServiceImpl.java
@@ -5,9 +5,12 @@
 
 package com.liferay.contact.management.service.impl;
 
+import com.liferay.contact.management.model.Contact;
 import com.liferay.contact.management.service.base.ContactLocalServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.ServiceContext;
 import org.osgi.service.component.annotations.Component;
 
 /**
@@ -18,4 +21,22 @@ import org.osgi.service.component.annotations.Component;
 	service = AopService.class
 )
 public class ContactLocalServiceImpl extends ContactLocalServiceBaseImpl {
+
+	public Contact addContact( String name, String email, long phone, String address, ServiceContext serviceContext) throws PortalException {
+
+		long contactId = counterLocalService.increment();
+
+		Contact entity = contactPersistence.create(contactId);
+
+		entity.setUuid(serviceContext.getUuid());
+		entity.setExpandoBridgeAttributes(serviceContext);
+		entity.setName(name);
+		entity.setEmail(email);
+		entity.setPhone(phone);
+		entity.setAddress(address);
+
+		contactPersistence.update(entity);
+
+		return entity;
+	}
 }
