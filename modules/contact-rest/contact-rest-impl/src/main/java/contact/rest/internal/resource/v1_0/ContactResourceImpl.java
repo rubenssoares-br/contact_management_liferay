c653a7251f019c3f4dd31eca782e7be30b0dfd19
Rubens Soares <rubens.soares@liferay.com>
2024-12-14 21:26:59 -0300

    Add Implementation for HTTP methods in ContactResourceImpl

--- modules/contact-rest/contact-rest-impl/src/main/java/contact/rest/internal/resource/v1_0/ContactResourceImpl.java
+++ modules/contact-rest/contact-rest-impl/src/main/java/contact/rest/internal/resource/v1_0/ContactResourceImpl.java
@@ -1,7 +1,9 @@
 package contact.rest.internal.resource.v1_0;
 
+import com.liferay.portal.vulcan.pagination.Page;
 import com.liferay.petra.function.UnsafeBiConsumer;
 import com.liferay.petra.function.UnsafeFunction;
+
 import contact.rest.dto.v1_0.Contact;
 import contact.rest.resource.v1_0.ContactResource;
 
@@ -20,9 +22,45 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = ContactResource.class
 )
 public class ContactResourceImpl extends BaseContactResourceImpl {
+	@Override
+	public Page<Contact> getContact() throws Exception {
+		return Page.of(_contacts.values());
+	}
 
 	@Override
-	public void setContextBatchUnsafeBiConsumer(UnsafeBiConsumer<Collection<Contact>, UnsafeFunction<Contact, Contact, Exception>, Exception> unsafeBiConsumer) {
-
+	public Contact getIdContact(Integer contactId) {
+		return _contacts.get(contactId);
 	}
+
+	@Override
+	public void deleteContact(Integer contactId) throws Exception {
+         _contacts.remove(contactId);
+    }
+	@Override
+	public Contact postContact(Contact contact) {
+		if (_contacts.containsKey(contact.getContactId())) {
+			throw new IllegalArgumentException("Duplicate contact ID " + contact.getContactId());
+		}
+
+		_contacts.put(contact.getContactId(), contact);
+
+		return contact;
+	}
+
+	@Override
+	public Contact putContact(Integer contactId, Contact contact) {
+		if (_contacts.containsKey(contact.getContactId())) {
+			throw new IllegalArgumentException("Duplicate contact ID " + contact.getContactId());
+		}
+
+		_contacts.put(contactId, contact);
+		
+		return contact;
+	}
+
+	public void setContextBatchUnsafeBiConsumer(UnsafeBiConsumer<Collection<Contact>, UnsafeFunction<Contact, Contact, Exception>, Exception> unsafeBiConsumer) {
+	}
+
+	private static final Map<Integer, Contact> _contacts = new Hashtable<Integer, Contact>();
+
 }
