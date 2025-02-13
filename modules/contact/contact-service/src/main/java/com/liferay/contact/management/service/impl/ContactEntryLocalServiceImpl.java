141f04261ef06d2b70e21d174a73be10564a5b6a
Rubens Soares <rubens.soares@liferay.com>
2025-02-13 10:04:58 -0300

    Add methods to validate parameters in ContactEntryLocalServiceImpl

--- modules/contact/contact-service/src/main/java/com/liferay/contact/management/service/impl/ContactEntryLocalServiceImpl.java
+++ modules/contact/contact-service/src/main/java/com/liferay/contact/management/service/impl/ContactEntryLocalServiceImpl.java
@@ -5,12 +5,16 @@
 
 package com.liferay.contact.management.service.impl;
 
+import com.liferay.contact.management.exception.ContactAddressException;
+import com.liferay.contact.management.exception.ContactEntryFamilyRelationshipException;
+import com.liferay.contact.management.exception.ContactPhoneException;
 import com.liferay.contact.management.model.ContactEntry;
 import com.liferay.contact.management.service.base.ContactEntryLocalServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.Validator;
 import org.osgi.service.component.annotations.Component;
 
 /**
@@ -24,6 +28,8 @@ public class ContactEntryLocalServiceImpl
 	extends ContactEntryLocalServiceBaseImpl {
 
 	public ContactEntry addContactEntry(String familyRelationship, long phone, String address, long contactId, ServiceContext serviceContext) throws PortalException {
+
+		_validateParameters(familyRelationship, phone, address, contactId);
 
 		long entryId = counterLocalService.increment();
 
@@ -65,5 +71,36 @@ public class ContactEntryLocalServiceImpl
 	public ContactEntry getContactEntry(long entryId) throws PortalException {
 
 		return contactEntryPersistence.findByPrimaryKey(entryId);
+	}
+
+	private void _validateParameters(String familyRelationship, long phone, String address, long contactId) throws PortalException {
+
+		_validateFamilyRelationship(familyRelationship);
+
+		_validatePhone(phone);
+
+		_validateAddress(address);
+
+	}
+
+	private void _validateFamilyRelationship(String familyRelationship) throws PortalException {
+
+		if (Validator.isNull(familyRelationship)) {
+			throw new ContactEntryFamilyRelationshipException("FamilyRelationship is null");
+		}
+	}
+
+	private void _validatePhone(long phone) throws PortalException {
+
+		if (Validator.isNull(phone)) {
+			throw new ContactPhoneException("Phone is null");
+		}
+	}
+
+	private void _validateAddress(String address) throws PortalException {
+
+		if (Validator.isBlank(address)) {
+			return;
+		}
 	}
 }
