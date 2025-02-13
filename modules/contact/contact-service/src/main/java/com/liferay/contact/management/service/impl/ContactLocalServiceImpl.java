978e6e28ecc51a19dff4f702d1733175848484ab
Rubens Soares <rubens.soares@liferay.com>
2025-02-13 09:52:28 -0300

    Add methods to validate parameters in ContactLocalServiceImpl

--- modules/contact/contact-service/src/main/java/com/liferay/contact/management/service/impl/ContactLocalServiceImpl.java
+++ modules/contact/contact-service/src/main/java/com/liferay/contact/management/service/impl/ContactLocalServiceImpl.java
@@ -6,12 +6,16 @@
 package com.liferay.contact.management.service.impl;
 
 
+import com.liferay.contact.management.exception.ContactEmailException;
+import com.liferay.contact.management.exception.ContactNameException;
+import com.liferay.contact.management.exception.ContactPhoneException;
 import com.liferay.contact.management.model.Contact;
 import com.liferay.contact.management.service.base.ContactLocalServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.service.ServiceContext;
 
+import com.liferay.portal.kernel.util.Validator;
 import org.osgi.service.component.annotations.Component;
 
 /**
@@ -24,6 +28,8 @@ import org.osgi.service.component.annotations.Component;
 public class ContactLocalServiceImpl extends ContactLocalServiceBaseImpl {
 
 	public Contact addContact( String name, String email, long phone, String address, ServiceContext serviceContext) throws PortalException {
+
+		_validateParameters(name, email, phone, address);
 
 		long contactId = counterLocalService.increment();
 
@@ -42,6 +48,8 @@ public class ContactLocalServiceImpl extends ContactLocalServiceBaseImpl {
 	}
 
 	public Contact updateContact( String name,long contactId, String email, long phone, String address, ServiceContext serviceContext) throws PortalException {
+
+		_validateParameters(name, email, phone, address);
 
 		Contact entity = contactPersistence.findByPrimaryKey(contactId);
 
@@ -66,6 +74,46 @@ public class ContactLocalServiceImpl extends ContactLocalServiceBaseImpl {
 	public Contact getContact(long contactId) throws PortalException {
 
 		return contactPersistence.findByPrimaryKey(contactId);
+	}
+
+	private void _validateParameters(String name, String email, long phone, String address) throws PortalException {
+
+		_validateName(name);
+
+		_validateEmailAddress(email);
+
+		_validateAddress(address);
+
+		_validatePhone(phone);
+
+	}
+
+	private void _validateName(String name) throws PortalException {
+
+		if (Validator.isNull(name)) {
+			throw new ContactNameException("Name is null");
+		}
+	}
+
+	private void _validateEmailAddress(String email) throws PortalException {
+
+		if (Validator.isNull(email)) {
+			throw new ContactEmailException("Email is null");
+		}
+	}
+
+	private void _validatePhone(long phone) throws PortalException {
+
+		if (Validator.isNull(phone)) {
+			throw new ContactPhoneException("Phone is null");
+		}
+	}
+
+	private void _validateAddress(String address) throws PortalException {
+
+		if (Validator.isNull(address)) {
+			return;
+		}
 	}
 
 }
