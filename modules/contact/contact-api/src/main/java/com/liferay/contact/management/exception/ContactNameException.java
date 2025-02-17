b243d64646d53e944dcbce9d0af25b2e2774fd13
Rubens Soares <rubens.soares@liferay.com>
2025-02-17 17:51:20 -0300

    Define static inner classes MustNotBeNull, MustOnlyContainLettersAndDigits, and MustBeLessThan50Characters, and add a constructor to the ContactNameException class that accepts a list of strings and a string as parameters

--- modules/contact/contact-api/src/main/java/com/liferay/contact/management/exception/ContactNameException.java
+++ modules/contact/contact-api/src/main/java/com/liferay/contact/management/exception/ContactNameException.java
@@ -6,16 +6,45 @@ package com.liferay.contact.management.exception;
 
 import com.liferay.portal.kernel.exception.PortalException;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author Rubens Soares
  */
 public class ContactNameException extends PortalException {
+
+	public static class MustNotBeNull extends ContactNameException {
+
+		public MustNotBeNull() {
+			super("Name is null");
+		}
+
+	}
+
+	public static class MustOnlyContainLettersAndDigits extends ContactNameException {
+
+		public MustOnlyContainLettersAndDigits() {
+			super("Name must only contain letters and digits");
+		}
+
+	}
+
+	public static class MustBeLessThan50Characters extends ContactNameException {
+		public MustBeLessThan50Characters() {
+			super(Arrays.asList(50, "name"), "Name must be less than 50 characters");
+		}
+	}
 
 	public ContactNameException() {
 	}
 
 	public ContactNameException(String msg) {
 		super(msg);
+	}
+
+	public ContactNameException(List<Object> argument, String message) {
+		super(message);
 	}
 
 	public ContactNameException(String msg, Throwable throwable) {
