09c03c263923b17b57642fde39282aca9d7d5685
Rubens Soares <rubens.soares@liferay.com>
2025-02-21 10:54:26 -0300

    Define static inner classes MustNotBeNull, MustNotBeDuplicate, MustBeLessThan60Characters, MustOnlyContainLettersAndDigits and new constructor in ContactAddressException

--- modules/contact/contact-api/src/main/java/com/liferay/contact/management/exception/ContactAddressException.java
+++ modules/contact/contact-api/src/main/java/com/liferay/contact/management/exception/ContactAddressException.java
@@ -6,15 +6,47 @@ package com.liferay.contact.management.exception;
 
 import com.liferay.portal.kernel.exception.PortalException;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author Rubens Soares
  */
 public class ContactAddressException extends PortalException {
 
+	public static class MustNotBeNull extends ContactAddressException {
+		public MustNotBeNull() {
+			super("Address is null");
+		}
+
+	}
+
+	public static class MustNotBeDuplicate extends ContactAddressException {
+		public MustNotBeDuplicate(String address) {
+			super("Address " + address + " duplicated");
+		}
+	}
+
+	public static class MustBeLessThan60Characters extends ContactAddressException {
+		public MustBeLessThan60Characters() {
+			super(Arrays.asList(60, "address"), "Address must be less than 60 characters");
+		}
+	}
+
+	public static class MustOnlyContainLettersAndDigits extends ContactAddressException {
+		public MustOnlyContainLettersAndDigits() {
+			super("Address must only contain letters and digits");
+		}
+	}
+
 	public ContactAddressException() {
 	}
 
 	public ContactAddressException(String msg) {
+		super(msg);
+	}
+
+	public ContactAddressException(List<Object> argument, String msg) {
 		super(msg);
 	}
 
