a947d24c2b15de87e982915650964108e5f00354
Rubens Soares <rubens.soares@liferay.com>
2025-02-21 15:42:37 -0300

    Define static inner classes MustBeLessThan50Characters, MustOnlyContainLettersAndDigits, MustNotBeNull and MustNotBeDuplicate in ContactEntryFamilyRelationshipException class

--- modules/contact/contact-api/src/main/java/com/liferay/contact/management/exception/ContactEntryFamilyRelationshipException.java
+++ modules/contact/contact-api/src/main/java/com/liferay/contact/management/exception/ContactEntryFamilyRelationshipException.java
@@ -6,15 +6,47 @@ package com.liferay.contact.management.exception;
 
 import com.liferay.portal.kernel.exception.PortalException;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author Rubens Soares
  */
 public class ContactEntryFamilyRelationshipException extends PortalException {
 
+	public static class MustNotBeDuplicate extends ContactEntryFamilyRelationshipException {
+		public MustNotBeDuplicate(String familyRelationship) {
+			super("Family Relationship with the name " + familyRelationship + " duplicated");
+		}
+	}
+
+	public static class MustNotBeNull extends ContactEntryFamilyRelationshipException {
+		public MustNotBeNull() {
+			super("Family Relationship is null");
+		}
+	}
+
+	public static class MustOnlyContainLettersAndDigits extends ContactEntryFamilyRelationshipException {
+		public MustOnlyContainLettersAndDigits() {
+			super("Name of the Family Relationship must only contain letters and digits");
+		}
+
+	}
+
+	public static class MustBeLessThan50Characters extends ContactEntryFamilyRelationshipException {
+		public MustBeLessThan50Characters() {
+			super(Arrays.asList(50, "familyRelationship"), "Family Relationship name must be less than 50 characters");
+		}
+	}
+
 	public ContactEntryFamilyRelationshipException() {
 	}
 
 	public ContactEntryFamilyRelationshipException(String msg) {
+		super(msg);
+	}
+
+	public ContactEntryFamilyRelationshipException(List<Object> argument, String msg) {
 		super(msg);
 	}
 
