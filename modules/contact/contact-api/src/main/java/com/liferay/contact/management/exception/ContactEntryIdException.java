54c076bdd29d117a4f79f167a6c6ef1318566a45
Rubens Soares <rubens.soares@liferay.com>
2025-02-26 09:22:19 -0300

    Define static inner classes MustNotBeNull, MustOnlyContainDigits, MustBeLessThan30Characters, MustBeValid and new constructor in ContactEntryIdException class

--- modules/contact/contact-api/src/main/java/com/liferay/contact/management/exception/ContactEntryIdException.java
+++ modules/contact/contact-api/src/main/java/com/liferay/contact/management/exception/ContactEntryIdException.java
@@ -6,15 +6,46 @@ package com.liferay.contact.management.exception;
 
 import com.liferay.portal.kernel.exception.PortalException;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author Rubens Soares
  */
 public class ContactEntryIdException extends PortalException {
 
+	public static class MustNotBeNull extends ContactEntryIdException {
+		public MustNotBeNull() {
+			super("EntryId is null");
+		}
+	}
+
+	public static class MustOnlyContainDigits extends ContactEntryIdException {
+		public MustOnlyContainDigits() {
+			super("EntryId must only contain digits");
+		}
+	}
+
+	public static class MustBeLessThan30Characters extends ContactEntryIdException {
+		public MustBeLessThan30Characters() {
+			super(Arrays.asList(30, "entryId"), "EntryId must be less than 30 characters");
+		}
+	}
+
+	public static class MustBeValid extends ContactEntryIdException {
+		public MustBeValid() {
+			super("EntryId not found in database");
+		}
+	}
+
 	public ContactEntryIdException() {
 	}
 
 	public ContactEntryIdException(String msg) {
+		super(msg);
+	}
+
+	public ContactEntryIdException(List<Object> argument, String msg) {
 		super(msg);
 	}
 
