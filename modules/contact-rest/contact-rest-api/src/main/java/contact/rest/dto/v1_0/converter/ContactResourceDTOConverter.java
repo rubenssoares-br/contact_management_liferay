02e8ef09c548ad46aea1f34a625d7b107d990675
Rubens Soares <rubens.soares@liferay.com>
2025-03-10 14:48:52 -0300

    Add toDTO method to ContactResourceDTOConverter class

--- modules/contact-rest/contact-rest-api/src/main/java/contact/rest/dto/v1_0/converter/ContactResourceDTOConverter.java
+++ modules/contact-rest/contact-rest-api/src/main/java/contact/rest/dto/v1_0/converter/ContactResourceDTOConverter.java
@@ -1,10 +1,9 @@
 package contact.rest.dto.v1_0.converter;
 
-import com.liferay.contact.management.service.ContactService;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
+import com.liferay.portal.vulcan.dto.converter.DTOConverterContext;
 import contact.rest.dto.v1_0.Contact;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Rubens Soares
@@ -16,11 +15,17 @@ public class ContactResourceDTOConverter implements DTOConverter<com.liferay.con
     @Override
     public String getContentType() {
         return Contact.class.getSimpleName();
-    }   
+    }
 
-
-
-    @Reference
-    private ContactService _contactService;
+    public Contact toDTO(DTOConverterContext dtoConverterContext,com.liferay.contact.management.model.Contact serviceBuilderContact) throws Exception {
+        return new Contact() {
+            {
+               setName(serviceBuilderContact::getName);
+               setEmail(serviceBuilderContact::getEmail);
+               setPhone(Math.toIntExact(serviceBuilderContact.getPhone()));
+               setAddress(serviceBuilderContact::getAddress);
+            }
+        };
+    }
 
 }
