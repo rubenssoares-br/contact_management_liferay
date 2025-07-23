18fc4302d5a3e2b44679468844b0e60eeabd4716
Rubens Soares <rubens.soares@liferay.com>
2025-07-23 11:34:53 -0300

    q3-2025-delivery add contact-web module

---
+++ modules/contact-web/src/main/java/com/liferay/contact/management/constants/ContactPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.contact.management.constants;
+
+/**
+ * @author me
+ */
+public class ContactPortletKeys {
+
+	public static final String CONTACT =
+		"com_liferay_contact_management_ContactPortlet";
+
+}
