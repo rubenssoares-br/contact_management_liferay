6ee068de4ab306866b78dc51a4e1c5135b9f5c5f
Rubens Soares <rubens.soares@liferay.com>
2025-07-31 17:58:15 -0300

    q3-2025-delivery Implement portlet action addContact - v1.0

--- modules/contact-web/src/main/java/com/liferay/contact/management/portlet/ContactPortlet.java
+++ modules/contact-web/src/main/java/com/liferay/contact/management/portlet/ContactPortlet.java
@@ -4,9 +4,17 @@ import com.liferay.contact.management.constants.ContactPortletKeys;
 
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 
-import javax.portlet.Portlet;
+import javax.portlet.*;
+
+import com.liferay.portal.kernel.util.ParamUtil;
 
 import org.osgi.service.component.annotations.Component;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author me
@@ -26,4 +34,48 @@ import org.osgi.service.component.annotations.Component;
 	service = Portlet.class
 )
 public class ContactPortlet extends MVCPortlet {
+
+	public void addContact(ActionRequest request, ActionResponse response) {
+		try {
+			PortletPreferences prefs = request.getPreferences();
+
+			String[] contactEntries = prefs.getValues("contact-entries", new String[1]);
+
+			ArrayList<String> entries = new ArrayList<>();
+
+			if (contactEntries[0] != null) {
+				entries = new ArrayList<>(Arrays.asList(prefs.getValues("contact-entries", new String[1])));
+			}
+
+			String name = ParamUtil.getString(request, "name");
+			String email = ParamUtil.getString(request, "email");
+			String phone = ParamUtil.getString(request, "phone");
+			String address = ParamUtil.getString(request, "address");
+
+			String entry = name + "^" + email + "^" + phone + "^" + address;
+
+			entries.add(entry);
+
+			String[] array = entries.toArray(new String[entries.size()]);
+
+			prefs.setValue("contact-entries", Arrays.toString(array));
+
+			try {
+				prefs.store();
+			}
+			catch (IOException e) {
+				Logger.getLogger(ContactPortlet.class.getName()).log(
+						Level.SEVERE, null, e);
+			}
+			catch (ValidatorException e) {
+				Logger.getLogger(ContactPortlet.class.getName()).log(
+						Level.SEVERE, null, e);
+			}
+
+
+		} catch (ReadOnlyException e) {
+			Logger.getLogger(ContactPortlet.class.getName()).log(
+					Level.SEVERE, null, e);
+        }
+    }
 }
