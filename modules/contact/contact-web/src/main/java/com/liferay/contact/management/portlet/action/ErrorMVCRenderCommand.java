34748010ee508c6cf204e2e5c96b48de0c5af0f6
Rubens Soares <rubens.soares@liferay.com>
2025-09-22 14:50:25 -0300

    Add ErrorMVCRenderCommand class in contact-web module

---
+++ modules/contact/contact-web/src/main/java/com/liferay/contact/management/portlet/action/ErrorMVCRenderCommand.java
@@ -0,0 +1,25 @@
+package com.liferay.contact.management.portlet.action;
+
+import com.liferay.contact.management.constants.ContactPortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.util.ParamUtil;
+import org.osgi.service.component.annotations.Component;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+@Component(
+        property = {
+                "javax.portlet.name=" + ContactPortletKeys.CONTACT,
+                "mvc.command.name=/contact/error"
+        },
+        service = MVCRenderCommand.class
+)
+public class ErrorMVCRenderCommand implements MVCRenderCommand {
+    @Override
+    public String render(RenderRequest renderRequest, RenderResponse renderResponse) throws PortletException {
+
+        return "/contactwebportlet/error.jsp";
+    }
+}
