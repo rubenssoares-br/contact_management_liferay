f086090004a56f16449906c3f79f8b54adcb7732
Rubens Soares <rubens.soares@liferay.com>
2025-10-03 10:39:11 -0300

    Add DeleteContactEntryMVCActionCommand class

---
+++ modules/contact/contact-web/src/main/java/com/liferay/contact/management/portlet/action/DeleteContactEntryMVCActionCommand.java
@@ -0,0 +1,49 @@
+package com.liferay.contact.management.portlet.action;
+
+import com.liferay.contact.management.constants.ContactPortletKeys;
+import com.liferay.contact.management.portlet.ContactPortlet;
+import com.liferay.contact.management.service.ContactEntryLocalService;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.util.ParamUtil;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Component(
+        property = {
+                "javax.portlet.name=" + ContactPortletKeys.CONTACT,
+                "mvc.command.name=/contact/deleteContact"
+        },
+        service = MVCActionCommand.class
+)
+public class DeleteContactEntryMVCActionCommand extends BaseMVCActionCommand {
+    @Override
+    protected void doProcessAction(ActionRequest actionRequest, ActionResponse actionResponse) throws Exception {
+
+        long entryId = ParamUtil.getLong(actionRequest, "entryId");
+
+        try {
+
+            _contactEntryLocalService.deleteContactEntry(entryId);
+
+            actionResponse.setRenderParameter(
+                    "mvcRenderCommandName", "/contact/addorupdatecontactEntry");
+        }
+
+        catch (Exception e) {
+            actionResponse.setRenderParameter(
+                    "mvcRenderCommandName", "/contact/error");
+
+            Logger.getLogger(ContactPortlet.class.getName()).log(
+                    Level.SEVERE, null, e);
+        }
+    }
+
+    @Reference
+    private ContactEntryLocalService _contactEntryLocalService;
+}
