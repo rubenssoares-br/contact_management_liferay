c5ed6814f847f70dfaba5a068ad442ce6493a272
Rubens Soares <rubens.soares@liferay.com>
2025-10-03 10:40:44 -0300

    Add implementation to processAction method in AddOrUpdateContactEntryMVCActionCommand class

--- modules/contact/contact-web/src/main/java/com/liferay/contact/management/portlet/action/AddOrUpdateContactEntryMVCActionCommand.java
+++ modules/contact/contact-web/src/main/java/com/liferay/contact/management/portlet/action/AddOrUpdateContactEntryMVCActionCommand.java
@@ -2,12 +2,14 @@ package com.liferay.contact.management.portlet.action;
 
 import com.liferay.contact.management.constants.ContactPortletKeys;
 import com.liferay.contact.management.model.ContactEntry;
+import com.liferay.contact.management.service.ContactEntryLocalService;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextFactory;
 import com.liferay.portal.kernel.util.ParamUtil;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -32,7 +34,38 @@ public class AddOrUpdateContactEntryMVCActionCommand extends BaseMVCActionComman
 
         if (entryId > 0) {
 
+            try {
+                _contactEntryLocalService.updateContactEntry(
+                        familyRelationship, entryId, phone, address, contactId, serviceContext);
+
+                actionResponse.setRenderParameter(
+                        "mvcRenderCommandName", "/contact/addorupdatecontactentry");
+            }
+            catch (Exception e) {
+                System.out.println(e);
+
+                actionResponse.setRenderParameter(
+                        "mvcRenderCommandName", "/contact/error");
+            }
+        } else {
+
+            try {
+                _contactEntryLocalService.addContactEntry(familyRelationship, phone, address, contactId, serviceContext);
+
+                actionResponse.setRenderParameter(
+                        "mvcRenderCommandName", "/contact/addorupdatecontactentry");
+            }
+            catch (Exception e) {
+                System.out.println(e);
+
+                actionResponse.setRenderParameter(
+                        "mvcRenderCommandName", "/contact/error");
+            }
+
         }
 
     }
+
+    @Reference
+    private ContactEntryLocalService _contactEntryLocalService;
 }
