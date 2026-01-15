37a0a5c7d3f14f0065c2c2964ca1a4055eac351d
Rubens Soares <rubens.soares@liferay.com>
2026-01-15 09:21:33 -0300

    Add ContactConstants class in contact-api module

---
+++ modules/contact/contact-api/src/main/java/com/liferay/contact/management/constants/ContactConstants.java
@@ -0,0 +1,10 @@
+package com.liferay.contact.management.constants;
+
+
+/**
+ * @author Rubens Soares
+ */
+public class ContactConstants {
+
+    public static final String RESOURCE_NAME = "com.liferay.contact.management";
+}
